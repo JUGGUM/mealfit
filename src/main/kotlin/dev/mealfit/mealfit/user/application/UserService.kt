ea85c3e1717c877c72bd0e3492f91ad0c2b5b1f7
JUGGUM <gayoung801@naver.com>
2025-07-28 10:24:41 +0900

    Update : UserCacheRepository 주석

--- src/main/kotlin/dev/mealfit/mealfit/user/application/UserService.kt
+++ src/main/kotlin/dev/mealfit/mealfit/user/application/UserService.kt
@@ -1,6 +1,5 @@
 package dev.mealfit.mealfit.user.application
 
-import dev.mealfit.mealfit.user.infrastructure.cache.UserCacheRepository
 import dev.mealfit.mealfit.user.infrastructure.persistence.UserRepository
 import dev.mealfit.mealfit.user.presentation.dto.UserDto
 import org.springframework.stereotype.Service
@@ -8,7 +7,7 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val userCacheRepository: UserCacheRepository
+    //private val userCacheRepository: UserCacheRepository
 ) {
 //    fun getUserDetail(userId: Long): UserDto {
 //        return userCacheRepository.getUser(userId)
