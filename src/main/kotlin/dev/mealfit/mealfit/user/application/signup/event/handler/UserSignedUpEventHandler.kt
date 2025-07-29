5674ef3049557fd62b92f7a650c8968baa2c90fc
JUGGUM <gayoung801@naver.com>
2025-07-29 15:50:38 +0900

    Update : application.yml

--- src/main/kotlin/dev/mealfit/mealfit/user/application/signup/event/handler/UserSignedUpEventHandler.kt
+++ src/main/kotlin/dev/mealfit/mealfit/user/application/signup/event/handler/UserSignedUpEventHandler.kt
@@ -1,8 +1,10 @@
 package dev.mealfit.mealfit.user.application.signup.event.handler
 
+import dev.mealfit.mealfit.diet.application.DietSurveyService
 import dev.mealfit.mealfit.user.domain.events.UserSignedUpEvent
 import dev.mealfit.mealfit.user.infrastructure.messaging.KafkaUserPayloadDto
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -11,11 +13,13 @@ import org.springframework.transaction.event.TransactionalEventListener
 @Component
 class UserSignedUpEventHandler(
     //private val kafkaTemplate: KafkaTemplate<String, KafkaUserPayloadDto>
+    private val dietSurveyService: DietSurveyService
 ) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     // DB 트랜잭션 커밋이후에 이벤트 핸들링
+    @Async("customExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handle(event: UserSignedUpEvent) {
         val payload = KafkaUserPayloadDto(
@@ -24,6 +28,7 @@ class UserSignedUpEventHandler(
             nickname = event.nickname,
             joinedAt = System.currentTimeMillis()
         )
+        dietSurveyService.createInitialSurvey(payload.userId, payload.nickname)
        // kafkaTemplate.send("user.signed-up", payload)
         logger.info("UserSignedUpEvent handled and sent to Kafka: $payload")
     }
