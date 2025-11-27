bd7f397ff2605b7459811225b90f0f23c77f26e2
JUGGUM <gayoung801@naver.com>
2025-11-27 11:32:39 +0900

    Feat : FoodInfo

---
+++ src/main/kotlin/dev/mealfit/mealfit/food/application/FoodInfo.kt
@@ -0,0 +1,9 @@
+package dev.mealfit.mealfit.food.application
+
+class FoodInfo {
+    private val name: String? = null
+    private val calories = 0
+    private val carbs = 0.0
+    private val protein = 0.0
+    private val fat = 0.0
+}
